b877d22fe0628a81354fac102b93a92c24964313
bamchoh <bamchoh@gmail.com>
2019-01-10 00:12:35 +0900

    Revise api server example to use websocket

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/bamchoh/pasori"
-	"github.com/gin-gonic/gin"
 )
+
+var addr = flag.String("addr", ":8080", "http service address")
 
 func dump_buffer(buf []byte) string {
 	str := ""
@@ -21,21 +22,29 @@ var (
 	PID uint16 = 0x06C3 // RC-S380
 )
 
-func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		fmt.Println("in")
-		idm, err := pasori.GetID(VID, PID)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err,
-			})
-			return
-		}
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
 
-		c.JSON(http.StatusOK, gin.H{
-			"id": dump_buffer(idm),
-		})
+func main() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
 	})
-	r.Run()
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAdnServe: ", err)
+	}
 }
