814c3301af439e467b38db2a549c05ebc22ccf14
bamchoh <bamchoh@gmail.com>
2019-01-12 02:39:06 +0900

    Refactor websocket behavior

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/bamchoh/pasori"
+	"github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -35,10 +40,57 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+func clientMain() {
+	log.SetFlags(0)
+
+	u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/ws"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+	idm := make(chan string)
+
+	go func() {
+		defer close(done)
+		for {
+			raw, err := pasori.GetID(VID, PID)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			idm <- dump_buffer(raw)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case idmstr := <-idm:
+			err := c.WriteMessage(websocket.TextMessage, []byte(idmstr))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
+	go func() {
+		for {
+			clientMain()
+		}
+	}()
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
